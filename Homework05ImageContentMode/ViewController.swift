d6ed6c5bdb1ccc24dc6c1c1151dcb361b57f487e
Poga Huang <pgh86812@gmail.com>
2021-11-13 20:58:53 +0800

    Initial Commit

---
+++ Homework05ImageContentMode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework05ImageContentMode
+//
+//  Created by 黃柏嘉 on 2021/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
